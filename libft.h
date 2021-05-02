9b5907401e1fe239238c22bd9381bff44c19a89b
Abh29 <aboutifor@stud.pkfu.ru>
2021-05-02 18:01:58 -0400

    adding putstr_fd

--- libft.h
+++ libft.h
@@ -41,6 +41,7 @@ char 	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
