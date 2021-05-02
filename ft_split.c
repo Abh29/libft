8988f9426558b0d8ece044a2be6ee75b912da009
Abh29 <aboutifor@stud.pkfu.ru>
2021-05-02 10:59:21 -0400

    adding split

---
+++ ft_split.c
@@ -0,0 +1,60 @@
+#include "libft.h"
+
+static int  ft_wordcount(char const *s, char c)
+{
+    int count;
+
+    count = 0;
+    while (*s)
+    {
+        while (*s && *s == c)
+            s++;
+        if (*s)
+        {
+            count++;
+            s++;
+        }
+        while (*s && *s != c)
+            s++;
+    }
+    return (count);
+}
+
+static void ft_helper(char **out, char const *s, char c)
+{
+    int i;
+    int w;
+    
+    w = 0;
+    while (*s)
+    {
+        while (*s && *s == c)
+            s++;
+        if (*s)
+        {
+            i = 0;
+            while (*s && *s != c && i++ > -1)
+                s++;
+            out[w] = malloc(i + 1);
+            s -= i;
+            i = 0;
+            while (*s && *s != c)
+                out[w][i++] = *s++;
+            out[w++][i] = '\0';
+        }
+    }
+    out[w] = 0;
+    }
+
+char        **ft_split(char const *s, char c)
+{
+    char    **out;
+
+    if (s == NULL)
+        return (NULL);
+    out = malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
+    if (out == NULL)
+        return (NULL);
+    ft_helper(out, s, c);
+    return (out);
+}
--- libft.h
+++ libft.h
@@ -35,6 +35,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char 	**ft_split(char const *s, char c);
+
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
