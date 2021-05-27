fd418b70d758c0bbbdb7bbe8f0babaf92128fa09
Abh29 <aboutifor@stud.pkfu.ru>
2021-05-27 07:41:31 -0400

    adding some funcs

--- ft_putnbr_base_fd_bonus.c
+++ ft_putnbr_base_fd_bonus.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-static void ft_print_nbr_base(int nb, const char *base, int size, char *buff, int fd)
+static void ft_print_nbr_base(unsigned int nb, const char *base, int size, char *buff, int fd)
 {
     int i;
     
@@ -29,15 +29,12 @@ void    ft_putnbr_base_fd(int nb, const char *base, int fd)
         write (fd, "0", 1);
         return ;
     }
+    if (nb < 0)
+      nb = (unsigned)(__UINT32_MAX__ + nb + 1);
     out = malloc (33);
     if (out == NULL)
         return ;
-    if (nb < 0)
-    {
-        write (fd, "-", 1);
-        nb *= -1;
-    }
-    ft_print_nbr_base(nb, base, base_size, out, fd);
+    ft_print_nbr_base((unsigned int)nb, base, base_size, out, fd);
     free(out);
     return ;
 }
--- ft_putnbr_unsigned_fd_bonus.c
+++ ft_putnbr_unsigned_fd_bonus.c
@@ -5,6 +5,7 @@ void    ft_putnbr_unsigned_fd(int nb, int fd)
     unsigned long k;
     char *buff;
     char i;
+    char *save;
 
     if (nb >= 0)
         ft_putnbr_fd(nb, fd);
@@ -13,19 +14,21 @@ void    ft_putnbr_unsigned_fd(int nb, int fd)
         buff = (char *)ft_calloc(12, sizeof(char));
         if (buff == NULL)
             return ;
+        save = buff;
         k = __UINT32_MAX__ + nb + 1;
         i = 0;
         while (k > 0)
         {
-            *buff++ = (char)(k % 10 + '0');
+            *save++ = (char)(k % 10 + '0');
             i++;
             k /= 10;
         }
         while (i-- >= 0)
         {
-            write (fd, buff, 1);
-            buff--;
+            write (fd, save, 1);
+            save--;
         }
+        save = NULL;
         free(buff);
     }
 }
